8435c7fa6e275bbd08a6aaba4d68e54111a07414
ChayanGuptaGithub <93777077+ChayanGuptaGithub@users.noreply.github.com>
2022-08-16 08:18:49 +0530

    Add files via upload

---
+++ power_of_num.py
@@ -0,0 +1,6 @@
+print("Program to find the power of expression a^b")
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+for i in range(1,b+1):
+    pow=a*a
+print("a^b=",pow)
